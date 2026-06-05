3a9d92286ca8b8b208c071589158df4750261e9b
Surekha <shindesurekha1101@example.com>
2026-06-05 11:51:30 +0200

    Remove unwanted folder

--- src/main/com/com/example/exception/RouteNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.example.exception;
-
-@SuppressWarnings("serial")
-public class RouteNotFoundException extends RuntimeException {
-
-	  public RouteNotFoundException(String message) {
-	        super(message);
-	    }
-}
